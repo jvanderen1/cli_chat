b842aadddc027b22464b21e5976b406ff58f1134
Joshua Van Deren <livetoscout@gmail.com>
2018-02-19 01:00:04 -0800

    Added build file for client

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "client/client.hpp"
+#include "client/admin.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout << "Hello World!" << endl;
+
+  return 0;
+}
